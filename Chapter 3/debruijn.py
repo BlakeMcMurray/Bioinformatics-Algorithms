21a7d430b3065272cec2eeeb5c80cb346d25108c
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 15:00:33 -0700

    Update debruijn.py

--- Chapter 3/debruijn.py
+++ Chapter 3/debruijn.py
@@ -5,39 +5,40 @@ Created on Wed Jan 24 13:31:07 2018
 
 @author: blakemcmurray
 """
-def Suffix(Dna):
+def suffix(Dna):
     word = Dna[1:]
     return(word)
     
-def Prefix(Dna):
+def prefix(Dna):
     word = Dna[0:len(Dna)-1]
     return(word)
     
-def elimDup(list):
+def elim_dup(list):
     noDup = []
     for k in range (len(list)):
         if list[k] not in noDup:
             noDup.append(list[k])
     return(noDup)   
     
-def compOfString(k, Text):
+def comp_of_s(k, Text):
     list = []
     for i in range(len(Text)-k+1):
         list.append(Text[i:i+k])
     return(list) 
   
-def deBruijnConstruction(k, Text):
-    edges = compOfString(k, Text)
-    deBruijnGraph = {}
+def debruijn_construction(k, Text):
+    edges = comp_of_s(k, Text)
+    graph = {}
     for i in range(len(edges)):
-        prefix = Prefix(edges[i])
-        suffix = Suffix(edges[i])
-        if prefix not in deBruijnGraph:
-            deBruijnGraph[prefix] = []
-            deBruijnGraph[prefix].append(suffix)
-        elif prefix in deBruijnGraph:
-            deBruijnGraph[prefix].append(suffix) 
-    for i in deBruijnGraph:
-        deBruijnGraph[i] = elimDup(deBruijnGraph[i])
+        prefix = prefix(edges[i])
+        suffix = suffix(edges[i])
+        if prefix not in graph:
+            graph[prefix] = []
+            graph[prefix].append(suffix)
+        elif prefix in graph:
+            graph[prefix].append(suffix) 
+    for i in graph:
+        graph[i] = elim_dup(graph[i])
         
-    return(deBruijnGraph)
+    return(graph)
+
