6cd214464273de2b6eb1ac947fe1cb928fbcc074
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 14:52:14 -0700

    Update chromToCycle.py

--- Chapter 6/chromToCycle.py
+++ Chapter 6/chromToCycle.py
@@ -5,12 +5,11 @@ Created on Mon Feb 19 18:15:55 2018
 
 @author: blakemcmurray
 """
-
-chromosome = (+1, -2,-3,4)
-def chromosometoCycle(chromosome):
+chrome = (+1, -2,-3,4)
+def chrome_to_cycle(chrome):
     s = []
-    for i in range(len(chromosome)):
-        j = chromosome[i]
+    for i in range(len(chrome)):
+        j = chrome[i]
         if(j < 0):
             s.append(2*(-j))
             s.append(2*(-j)-1)
@@ -19,12 +18,9 @@ def chromosometoCycle(chromosome):
             s.append(2*(j))
     return(s)
 
-breakList = ""
-with open(r'/Users/blakemcmurray/Downloads/rosalind_ba6f.txt') as f:
-    breakList = f.read().replace('(','').replace(')','').replace(' ',', ')
     
-chromosome = (+1, +2, +3, -4, -5, +6, +7, -8, +9, +10, -11, -12, +13, +14, -15, +16, -17, -18, +19, -20, +21, +22, +23, +24, +25, -26, +27, +28, -29, -30, -31, -32, -33, -34, +35, -36, +37, +38, +39, +40, -41, +42, -43, -44, -45, -46, +47, -48, -49, +50, +51, +52, +53, +54, -55, -56, -57, +58, +59, -60, +61, -62, -63, +64, -65, -66)
-cycle = chromosometoCycle(chromosome)
+chrome = (+1, +2, +3, -4, -5, +6, +7, -8, +9, +10, -11, -12, +13, +14, -15, +16, -17, -18, +19, -20, +21, +22, +23, +24, +25, -26, +27, +28, -29, -30, -31, -32, -33, -34, +35, -36, +37, +38, +39, +40, -41, +42, -43, -44, -45, -46, +47, -48, -49, +50, +51, +52, +53, +54, -55, -56, -57, +58, +59, -60, +61, -62, -63, +64, -65, -66)
+cycle = chrome_to_cycle(chrome)
 
 string1 = ""
 for i in cycle:
