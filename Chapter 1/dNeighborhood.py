ce329e868e592a44b28b6670659bef32eb396529
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 15:18:25 -0700

    Update dNeighborhood.py

--- Chapter 1/dNeighborhood.py
+++ Chapter 1/dNeighborhood.py
@@ -1,27 +1,27 @@
 #Working code
-def immediateNeighbors(Pattern):
-    Neighborhood = []
-    Neighborhood.append(Pattern)
+def immediate_neighbors(pattern):
+    neighborhood = []
+    neighborhood.append(pattern)
     letters = ["A","C","G","T"]
-    for i in range(len(Pattern)):
-        k = Pattern[i]
+    for i in range(len(pattern)):
+        k = pattern[i]
         for j in letters:
             if(j != k):
-                toAdd = list(Pattern)
+                toAdd = list(pattern)
                 toAdd[i] = j
-                Neighborhood.append(''.join(toAdd))
-    return(Neighborhood)
+                neighborhood.append(''.join(toAdd))
+    return(neighborhood)
 
-def iterativeNeighbors(Pattern,d):
-    Neighborhood = [Pattern]
+def iterative_neighbors(pattern,d):
+    neighborhood = [pattern]
     m = 0
     for j in range(0,d):
-        for i in Neighborhood:
+        for i in neighborhood:
             if(m == d):
                 break
-            immNeigh = immediateNeighbors(i)
-            for k in immNeigh:
-                Neighborhood.append(k)
-            Neighborhood = list(dict.fromkeys(Neighborhood))
+            imm_neigh = immediate_neighbors(i)
+            for k in imm_neigh:
+                neighborhood.append(k)
+            neighborhood = list(dict.fromkeys(neighborhood))
             m += 1 
-    return(Neighborhood)
+    return(neighborhood)
