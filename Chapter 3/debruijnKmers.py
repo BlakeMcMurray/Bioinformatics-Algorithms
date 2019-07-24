cda77ebd8d86c61417cde19706ee684fdf3d1868
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 15:05:30 -0700

    Update debruijnKmers.py

--- Chapter 3/debruijnKmers.py
+++ Chapter 3/debruijnKmers.py
@@ -1,51 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 24 21:07:47 2018
+Created on Wed Jan 24 13:31:07 2018
 
 @author: blakemcmurray
 """
-def Suffix(Dna):
+def suffix(Dna):
     word = Dna[1:]
     return(word)
     
-def Prefix(Dna):
+def prefix(Dna):
     word = Dna[0:len(Dna)-1]
     return(word)
     
-def elimDup(list):
+def elim_dup(list):
     noDup = []
     for k in range (len(list)):
         if list[k] not in noDup:
             noDup.append(list[k])
     return(noDup)   
     
-def compOfString(k, Text):
+def comp_of_s(k, Text):
     list = []
     for i in range(len(Text)-k+1):
         list.append(Text[i:i+k])
     return(list) 
   
-Patterns = []
+patterns = []
 with open(r'/Users/blakemcmurray/Downloads/rosalind_ba3e.txt') as f:
  
     for line in f:   
         l = line.strip()
-        Patterns.append(l)
+        patterns.append(l)
         
 f.close()
 
-def deBruijnKmer(Patterns):
-    edges = Patterns
-    deBruijnGraph = {}
+def debruijn_kmers(patterns):
+    edges = patterns
+    graph = {}
     for i in range(len(edges)):
-        prefix = Prefix(edges[i])
-        suffix = Suffix(edges[i])
-        print(edges[i])
-        if prefix not in deBruijnGraph:
-            deBruijnGraph[prefix] = []
-            deBruijnGraph[prefix].append(suffix)
-        elif prefix in deBruijnGraph:
-            deBruijnGraph[prefix].append(suffix) 
+        prefix = prefix(edges[i])
+        suffix = suffix(edges[i])
+        if prefix not in graph:
+            graph[prefix] = []
+            graph[prefix].append(suffix)
+        elif prefix in graph:
+            graph[prefix].append(suffix) 
         
-    return(deBruijnGraph)
+    return(graph)
