7738933a31a2b24bffdc2434e4c8a2d2082fe589
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 20:25:18 -0700

    Update minD.py

--- Chapter 2/minD.py
+++ Chapter 2/minD.py
@@ -1,17 +1,15 @@
+    
 import hamming as ham
-#return the kmer in Text that minimizes the hamming dist between
+#return the kmer in text that minimizes the hamming dist between
 #pattern and kmer as well as the hamming dist
 #-----working-----
-def minD(Pattern,Text):
-    k = len(Pattern)
-    min = len(Pattern)
+def min_d(pattern,text):
+    k = len(pattern)
+    min = len(pattern)
     kmer = ""
-    for i in range(len(Text)-k+1):
-        kmer = Text[i:i+k]
-        dist = ham.hammingDist(Pattern,kmer)
+    for i in range(len(text)-k+1):
+        kmer = text[i:i+k]
+        dist = ham.hamming_dist(pattern,kmer)
         if(dist < min):
             min = dist
     return(min)
-
-
-
