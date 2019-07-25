90e093b3168f11ccf71419ee8f0beca0b7749497
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 22:49:45 -0700

    Update approxOccurence.py

--- Chapter 1/approxOccurence.py
+++ Chapter 1/approxOccurence.py
@@ -1,6 +1,6 @@
 import hamming as ham
 def approx_occurence(text,pattern,d):
     for i in range(len(text)-len(pattern)+1):
-        if(ham.hammingDist(pattern,text[i:i+len(pattern)]) <= d):
+        if(ham.hamming_dist(pattern,text[i:i+len(pattern)]) <= d):
             return(True)
     return(False)
