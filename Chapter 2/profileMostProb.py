8aade4de54d66477de2b220a74ef8fd08da30f0e
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 20:31:46 -0700

    Update profileMostProb.py

--- Chapter 2/profileMostProb.py
+++ Chapter 2/profileMostProb.py
@@ -1,48 +1,25 @@
 import profileMatrix as pm 
 import letterMap as lm 
-def profMostProb(Text,k,profile):
-    mostProbKmer = Text[0:k]
-    bestProb = 0
+def prof_most_prob(text,k,profile):
+    most_prob_kmer = text[0:k]
+    best_prob = 0
     prob = 1
-    for i in range(len(Text)-k+1):
-        kmer = Text[i:i+k]
+    for i in range(len(text)-k+1):
+        kmer = text[i:i+k]
         for j in range(len(kmer)):
-            index = lm.letterMap(kmer[j])
+            index = lm.letter_map(kmer[j])
             prob = prob*profile[j][index]
-        if(prob > bestProb):
-            bestProb = prob
-            mostProbKmer = kmer
+        if(prob > best_prob):
+            best_prob = prob
+            most_prob_kmer = kmer
         prob = 1
-    return(mostProbKmer)
+    return(most_prob_kmer)
             
 
-pseduoProf = []
-
-'''with open("in.txt","r") as f:
-    lines = f.readlines()
-    Text = lines[0].strip()
-    k = int(lines[1])
-    for i in range(2,len(lines)):
-        pseduoProf.append(lines[i].split())
-f.close()
-
-profile = []
-for i in range(len(pseduoProf[0])):
-    profile.append([])
-
-
-for i in range(len(pseduoProf[0])):
-    profile[i].append(float(pseduoProf[0][i]))
-    profile[i].append(float(pseduoProf[1][i]))
-    profile[i].append(float(pseduoProf[2][i]))
-    profile[i].append(float(pseduoProf[3][i]))'''
-
-
-#print(profMostProb(Text,k,profile))
 
 
 
-   
+
 
 
 
