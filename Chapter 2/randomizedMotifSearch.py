e6eaa01d18acd4c3056c4d33bedc576cea4c67ff
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 20:34:32 -0700

    Update randomizedMotifSearch.py

--- Chapter 2/randomizedMotifSearch.py
+++ Chapter 2/randomizedMotifSearch.py
@@ -2,29 +2,29 @@ import motifs
 import random
 import score
 #working
-def randomizedMotifSearch(Dna,k,t):
+def randomized_motif_search(dna,k,t):
     kmers = []
-    for i in Dna:
+    for i in dna:
         r = random.randint(0,len(i)-k)
         kmers.append(i[r:r+k])
     while(True):
-        prof = motifs.pmp.pm.profileMatrix(kmers)
-        nKmers = motifs.motifs(prof,Dna)
-        if(score.score(nKmers) < score.score(kmers)):
-            kmers = nKmers
+        prof = motifs.pmp.pm.profile_matrix(kmers)
+        n_kmers = motifs.motifs(prof,dna)
+        if(score.score(n_kmers) < score.score(kmers)):
+            kmers = n_kmers
         else:
             return(kmers)
 
-def randomizedMotifSearch1000(Dna,k,t):
+def randomized_motif_search1000(dna,k,t):
     kmers = []
-    for i in Dna:
+    for i in dna:
         r = random.randint(0,len(i)-k+1)
         kmers.append(i[r:r+k])
 
     for i in range(1000):
-        nKmers = randomizedMotifSearch(Dna,k,t)
-        if(score.score(nKmers) < score.score(kmers)):
-            kmers = nKmers
+        n_kmers = randomized_motif_search(dna,k,t)
+        if(score.score(n_kmers) < score.score(kmers)):
+            kmers = n_kmers
     return(kmers)
 
 with open("in.txt","r") as f:
@@ -33,13 +33,9 @@ with open("in.txt","r") as f:
         lines[i] = lines[i].strip()
 f.close()
 
-rand = randomizedMotifSearch1000(lines,15,20)
+rand = randomized_motif_search1000(lines,15,20)
 
 with open("out.txt","w") as f:
     for i in rand:
         f.write(i + "\n")
 f.close()  
-
-
-
-
