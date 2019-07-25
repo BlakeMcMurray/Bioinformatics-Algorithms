55f7d36c51f9228dae631b71178000d02df42d5e
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 23:14:10 -0700

    Update patternMatch.py

--- Chapter 1/patternMatch.py
+++ Chapter 1/patternMatch.py
@@ -1,5 +1,5 @@
-def patternMatch(Pattern,Text):
-    for i in range(len(Text)-len(Pattern)+1):
-        if(Text[i:i+len(Pattern)] == Pattern):
+def pattern_match(pattern,text):
+    for i in range(len(text)-len(pattern)+1):
+        if(text[i:i+len(pattern)] == pattern):
             return(True)
     return(False)
