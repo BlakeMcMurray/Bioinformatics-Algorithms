f2360d832bdfcbf809a2c033ab96d7090f72bd00
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 20:20:34 -0700

    Update d.py

--- Chapter 2/d.py
+++ Chapter 2/d.py
@@ -1,9 +1,9 @@
 import minD
 #working
 #returns the minimum score + the motifs that give the min score
-def d(Pattern,Motifs):
-    sum = 0
-    for i in Motifs:
-        dCall = minD.minD(Pattern,i)
-        sum += dCall        
-    return(sum)
+def d(pattern,motifs):
+    summ = 0
+    for i in motifs:
+        d_call = minD.minD(pattern,i)
+        summ += d_call        
+    return(summ)
