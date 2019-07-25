30a3f0d757bd52609e0691c88eb39606d8300cb0
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 20:21:53 -0700

    Update hamming.py

--- Chapter 2/hamming.py
+++ Chapter 2/hamming.py
@@ -1,7 +1,6 @@
-def hammingDist(st1,st2):
+def hamming_dist(st_1,st_2):
     dist = 0
-    for i in range(len(st1)):
-        if(st1[i] != st2[i]):
+    for i in range(len(st_1)):
+        if(st_1[i] != st_2[i]):
             dist += 1
     return(dist)
-
