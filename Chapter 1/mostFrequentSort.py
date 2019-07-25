c18c04f31adc44378b72e80bbcceea5197a5eded
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 23:08:27 -0700

    Update mostFrequentSort.py

--- Chapter 1/mostFrequentSort.py
+++ Chapter 1/mostFrequentSort.py
@@ -1,6 +1,6 @@
 import math
 import numpy
-def numbertoPattern(num,k):
+def number_to_pattern(num,k):
     pattern = []
     for i in reversed(range(0,k)):
         n = math.floor(num/(4**i))
@@ -16,11 +16,11 @@ def numbertoPattern(num,k):
         elif(n == 3):
             pattern.append('T')
             num = num - 3*4**i
+    s_pattern = ''.join(pattern)
+    return(s_pattern)
 
-    toReturn = ''.join(pattern)
-    return(toReturn)
 
-def patternToNumber(pattern):
+def pattern_to_number(pattern):
     num = 0
     for i in reversed(range(len(pattern))):
         if(pattern[i] == "A"):
@@ -33,30 +33,28 @@ def patternToNumber(pattern):
             num = num + 3*4**(len(pattern)-i-1)
     return(num)
 
-def mostFrequentSort(Text,k):
-    index = [0]*(len(Text)-k+1)
-    count = [1]*(len(Text)-k+1)
+def most_frequent_sort(text,k):
+    index = [0]*(len(text)-k+1)
+    count = [1]*(len(text)-k+1)
     words = []
 
-    for i in range(len(Text)-k+1):
-        index[i] = patternToNumber(Text[i:i+k])
-    sortedIndex = sorted(index)
+    for i in range(len(text)-k+1):
+        index[i] = pattern_to_number(text[i:i+k])
+    sorted_index = sorted(index)
 
     #find the max run
-    maxRun = 0
-    for i in range(1,len(sortedIndex)):
-        if(sortedIndex[i-1] == sortedIndex[i]):
+    max_run = 0
+    for i in range(1,len(sorted_index)):
+        if(sorted_index[i-1] == sorted_index[i]):
             count[i] = count[i-1]+1
 
     for i in range(len(count)):
-        if(maxRun < count[i]):
-            maxRun = count[i]
+        if(max_run < count[i]):
+            max_run = count[i]
 
     #add words that have max Run
     for i in range(len(count)):
-        if(count[i] == maxRun):
-            words.append(numbertoPattern(sortedIndex[i],k))
+        if(count[i] == max_run):
+            words.append(number_to_pattern(sorted_index[i],k))
     
     return(words)
-
-
