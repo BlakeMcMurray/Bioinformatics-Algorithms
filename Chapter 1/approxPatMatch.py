421270db90822faa44843481a786bdaf8314ac84
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 22:50:25 -0700

    Update approxPatMatch.py

--- Chapter 1/approxPatMatch.py
+++ Chapter 1/approxPatMatch.py
@@ -3,8 +3,6 @@ import hamming
 def approx_match(pattern,text,d):
     sPositions = []
     for i in range(len(text)-len(pattern)+1):
-        if(hamming.hammingDist(pattern,text[i:i+len(pattern)]) <= d):
+        if(hamming.hamming_dist(pattern,text[i:i+len(pattern)]) <= d):
             sPositions.append(i)
     return(sPositions)
-
-
