db910a2131a95568497a04286f12dc90fe049b6f
BlakeMcMurray <blake.mcmurray@gmail.com>
2019-07-24 20:36:02 -0700

    Update score.py

--- Chapter 2/score.py
+++ Chapter 2/score.py
@@ -3,21 +3,17 @@ import countMatrix as CM
 #calculates the score of a set of motifs
 #breaks ties by first encounter of max nucleotide
 def score(kmers):
-    countM = CM.count(kmers)
+    count_m = CM.count(kmers)
     m = 0
     score = 0
     tracker = 0
-    for i in range(len(countM)):
-        m = max(countM[i])
-        for j in range(len(countM[i])):
-            if countM[i][j] == m and tracker == 0:
+    for i in range(len(count_m)):
+        m = max(count_m[i])
+        for j in range(len(count_m[i])):
+            if count_m[i][j] == m and tracker == 0:
                 tracker = 1
                 continue
             else:
-                score += countM[i][j]
+                score += count_m[i][j]
         tracker = 0
     return(score)
-
-    
-
-
